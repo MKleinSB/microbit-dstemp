06612fe7f2bf9152d055da3b2ed89c1b67d90bb5
Michael Klein <37311672+MKleinSB@users.noreply.github.com>
2022-05-22 18:20:02 +0200

    Aktualisiere test.ts

--- test.ts
+++ test.ts
@@ -1,8 +1,3 @@
-serial.writeLine("starting...")
-serial.writeLine("Press A to show temperature")
-serial.writeLine("If sensor is connected it should show the temperature")
-serial.writeLine("If sensor is NOT connected it should show -Infinity (for temp) and an error message")
-
 dstemp.sensorError(function (errorMessage, errorCode, port) {
     serial.writeString("\n");
     serial.writeString(errorMessage);
